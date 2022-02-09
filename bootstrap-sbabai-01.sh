e92429edeb368919666ec6489c618590a85297b9
Sasan Babai <sbabai@genetec.com>
2022-02-09 11:59:48 -0500

    renamed bootstrap.sh to bootstrap-sbabai-01.sh

--- bootstrap-sbabai-01.sh
+++ bootstrap-sbabai-01.sh
@@ -9,6 +9,13 @@ flux create kustomization infrastructure --source=flux-system --path=./infrastru
 
 mkdir -p infrastructure/kyverno
 
+# syncs with git repo, container registry, bucket (checksum is used as artifact id)
+# polls the source in specified interval
+# artifact is created in the cluster and cached
+# sync will happen even if the source is disconnected
+# image sync involves image reflector and image automation controllers
+# image reflector will watch registry for changes
+# image automation watches image reflector and applies the image policy and patches the repo
 flux create source git kyverno --url=ssh://git@github.com/sasanbabai/kyverno --tag-semver=">=1.0.0" --secret-ref=flux-system --interval=5m --export > infrastructure/kyverno/source.yaml
 
 git add -A && git commit -m "added keverno git repo" && git push origin main
@@ -25,10 +32,16 @@ flux reconcile kustomization flux-system --with-source
 
 flux get kustomizations
 
+# kustomizations also allow you to define dependency trees (depends-on)
 flux create kustomization apps --depends-on=infrastructure --source=flux-system --path=./apps --prune=true --interval=5m --export > clusters/sbabai-01/apps.yaml
 
 mkdir -p apps/podinfo
 
+# create apps namespace
+# create sbabai service account
+# bind sbabai with cluster-admin in apps namespace
+# create podinfo gitrepo in apps namespace
+# patch podinfo to include security context
 flux create tenant sbabai --with-namespace=apps --export > apps/sbabai.yaml
 
 kubectl create secret generic apps --namespace=apps --from-file=/home/sbabai/.ssh/identity --from-file=/home/sbabai/.ssh/identity.pub --from-file=/home/sbabai/.ssh/known_hosts
